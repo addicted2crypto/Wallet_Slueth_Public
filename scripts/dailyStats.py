4a3c2cc97bf9ece4f21e4096232278796d211777
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-08-19 15:52:39 -0700

    delete failure

--- scripts/dailyStats.py
+++ scripts/dailyStats.py
@@ -25,24 +25,3 @@ for curr in currencies:
     get_daily_metrics(curr)
 
 
-# def get_active_addresses_last_48_hours(chain):
-#     url = metrics_url_template.format(chain=chain)
-#     response = requests.get(url, headers=headers)
-#     if response.status_code == 200:
-#         data = response.json()
-        
-#         # Calculate date 48 hours ago
-#         now = datetime.utcnow()
-#         cutoff_date = now - timedelta(hours=48)
-        
-#         print(f"\n{chain} active addresses in the last 48 hours:")
-#         for entry in data:
-            
-#             entry_date = datetime.strptime(entry['time_period_start'][:19], "%Y-%m-%dT%H:%M:%S")
-#             if entry_date >= cutoff_date:
-#                 print(f"Date: {entry_date}, Active Addresses: {entry['value']}")
-#     else:
-#         print(f"Error fetching active addresses for {chain}: {response.status_code} - {response.text}")
-
-# for chain in chains:
-#     get_active_addresses_last_48_hours(chain)
